aa39b0899b7971a883b489f6aec9eb478002bbce
thitanun.mahapornchai@gmail.com <s6301012630036@email.kmutnb.ac.th>
2023-10-10 22:27:40 +0700

    Update Contract Address and Client ID

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,8 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  // const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x298DE4c372ce0Eb6725Bdc89DF2212917259B2fE');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
+// import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
 
 import { StateContextProvider } from './context';
 import App from './App';
 import './index.css';
 
+import { Sepolia } from "@thirdweb-dev/chains";
+import { ThirdwebProvider, useContract } from "@thirdweb-dev/react";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Goerli}> 
+  <ThirdwebProvider activeChain={ Sepolia } 
+  clientId="f3afba51ef95babba007e8b940f47ad7"> 
     <Router>
       <StateContextProvider>
         <App />
