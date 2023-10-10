525f1573fe01c774437cf90dd1184ffe15707249
thitanun.mahapornchai@gmail.com <s6301012630036@email.kmutnb.ac.th>
2023-10-11 01:03:52 +0700

    Update UI about show CountBox

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -41,20 +41,21 @@ const CampaignDetails = () => {
     <div>
       {isLoading && <Loader />}
 
-      <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+      <div className="flex md:h-[150px] w-full flex-nowrap justify-between gap-[10px]">
+          <CountBox title="Days Left" value={remainingDays} />
+          <CountBox title={`Raised of ${state.target}`} value={state.amountCollected} />
+          <CountBox title="Total Backers" value={donators.length} />
+        </div>
+
+      <div className="w-full flex md:flex-row flex-col mt-1 gap-[30px]">
         <div className="flex-1 flex-col">
-          <img src={state.image} alt="campaign" className="w-full h-[410px] object-cover rounded-xl"/>
+          <img src={state.image} alt="campaign" className="w-full h-[500px] object-cover rounded-xl"/>
           <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
             <div className="absolute h-full bg-[#4acd8d]" style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth: '100%'}}>
             </div>
           </div>
         </div>
 
-        <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
-          <CountBox title="Days Left" value={remainingDays} />
-          <CountBox title={`Raised of ${state.target}`} value={state.amountCollected} />
-          <CountBox title="Total Backers" value={donators.length} />
-        </div>
       </div>
 
       <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
