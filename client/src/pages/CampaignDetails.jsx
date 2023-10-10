2217abcd86d978ea65499e95dea6de6f52b1404b
thitanun.mahapornchai@gmail.com <s6301012630036@email.kmutnb.ac.th>
2023-10-11 02:49:56 +0700

    Update pop-up to show that the campaign target has been reached

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -1,11 +1,9 @@
-import React, { useState, useEffect } from 'react'
-import { useLocation, useNavigate } from 'react-router-dom';
-import { ethers } from 'ethers';
-
-import { useStateContext } from '../context';
-import { CountBox, CustomButton, Loader } from '../components';
+import React, { useState, useEffect } from 'react';
+import { Loader, CountBox, CustomButton } from '../components';
 import { calculateBarPercentage, daysLeft } from '../utils';
 import { thirdweb } from '../assets';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useStateContext } from '../context';
 
 const CampaignDetails = () => {
   const { state } = useLocation();
@@ -20,44 +18,46 @@ const CampaignDetails = () => {
 
   const fetchDonators = async () => {
     const data = await getDonations(state.pId);
-
     setDonators(data);
-  }
+  };
 
   useEffect(() => {
-    if(contract) fetchDonators();
-  }, [contract, address])
+    if (contract) fetchDonators();
+
+    if (state.amountCollected >= state.target && remainingDays !== 0) {
+      window.alert(`The campaign target has been reached! : ${state.target}!`);
+    }
+  }, [contract, address, state.target, remainingDays]);
 
   const handleDonate = async () => {
     setIsLoading(true);
-
-    await donate(state.pId, amount); 
-
-    navigate('/')
+    await donate(state.pId, amount);
+    navigate('/');
     setIsLoading(false);
-  }
-
+  };
+    
   return (
     <div>
+      
       {isLoading && <Loader />}
-
+      
       <div className="flex md:h-[150px] w-full flex-nowrap justify-between gap-[10px]">
-          <CountBox title="Days Left" value={remainingDays} />
+          <CountBox title="Days Left" value={remainingDays}/> 
           <CountBox title={`Raised of ${state.target}`} value={state.amountCollected} />
-          <CountBox title="Total Backers" value={donators.length} />
+          <CountBox title="Total Backers" value={donators.length} /> 
         </div>
-
-      <div className="w-full flex md:flex-row flex-col mt-1 gap-[30px]">
+       
+      <div className="w-full flex md:flex-row flex-col mt-1 gap-[30px]"> 
         <div className="flex-1 flex-col">
-          <img src={state.image} alt="campaign" className="w-full h-[500px] object-cover rounded-xl"/>
+          <img src={state.image} alt="campaign" className="w-full h-[410px] object-cover rounded-xl"/>
           <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
             <div className="absolute h-full bg-[#4acd8d]" style={{ width: `${calculateBarPercentage(state.target, state.amountCollected)}%`, maxWidth: '100%'}}>
             </div>
           </div>
         </div>
-
-      </div>
-
+        </div>
+        
+      
       <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
         <div className="flex-[2] flex flex-col gap-[40px]">
           <div>
